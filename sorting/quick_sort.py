80a536a98a2d6ac6aa99d03585ce97f243376a22
Priyanshu <priyanshuserver55@gmail.com>
2022-10-23 21:51:01 +0530

    Added gitignore and changed the quickSort get_pivot function little one

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -7,10 +7,10 @@ def get_pivot(arr,low,high):
         if arr[j] <= pivot:
             i += 1
             arr[i],arr[j] = arr[j],arr[i]
+    i+=1
+    arr[i],arr[high] = arr[high],arr[i]
 
-    arr[i+1],arr[high] = arr[high],arr[i+1]
-
-    return i + 1
+    return i
 
 def quickSort(arr,low,high):
     if low<high:
