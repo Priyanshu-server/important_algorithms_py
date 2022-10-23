f074d933c11352a285e497e65ecefb79000713cd
Priyanshu <priyanshuserver55@gmail.com>
2022-10-23 21:45:21 +0530

    Quick Sort added ! (without comments)

---
+++ sorting/quick_sort.py
@@ -0,0 +1,25 @@
+def get_pivot(arr,low,high):
+    pivot = arr[high]
+
+    i = low-1
+
+    for j in range(low,high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i],arr[j] = arr[j],arr[i]
+
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+
+    return i + 1
+
+def quickSort(arr,low,high):
+    if low<high:
+        pi = get_pivot(arr,low,high)
+
+        quickSort(arr,low,pi-1)
+        quickSort(arr,pi+1,high)
+
+
+arr = [5,3,4,1,2]
+quickSort(arr,0,len(arr)-1)
+print(*arr)
